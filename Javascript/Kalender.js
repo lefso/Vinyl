a2380153866c60dda458cfbcc3214f6f2fc00fe0
Therese Federl <theresehfederl@gmail.com>
2016-11-09 17:13:24 +0100

    lagt til timer i javascript

---
+++ Javascript/Kalender.js
@@ -0,0 +1,41 @@
+
+function resterendeTid(slutt) { 
+    let totalTid = Date.parse(slutt) - Date.now(); /*beregner millisekunder fra nå til slutt*/
+    let timer = Math.floor(totalTid/(1000*60*60)); /*gjør om millisekunder til timer*/
+    let dager = Math.floor(totalTid/(1000*60*60*24));  /*gjør om anntall millisekunder til dager*/
+    return {
+        'Total': totalTid,
+        'Timer': timer,
+        'Dager': dager
+    }; /*generer en liste med objekter med verdier som tilsvarer tiden mellom nå og sluttid*/
+}
+
+function startKlokke(sluttTid) {
+    const klokke = document.getElementById("timer");
+    klokke.style.display = "block";
+    let dagerSpan = klokke.querySelector(".dager");
+    function oppdater() {
+        let restT = resterendeTid(sluttTid);
+        dagerSpan.innerHTML = restT.Dager;
+        if (restT.Total>=0) {
+            clearInterval(); /*tidsintervall*/
+        }
+    }
+    oppdater();
+    let tidsintervall = setInterval(oppdater,1000);
+}
+
+const kommendeArrangement = [["Nov 6 2016", "Dec 10 2016"], ["Dec 11 2016", "Jan 7 2017"], ["Jan 8 2017", "Feb 4 2017"], ["Feb 5 2017", "Mar 3 2017"], ["Mar 4 2017", "Apr 29 2017"]];
+
+for (let i = 0; i<kommendeArrangement.length; i++){
+    let start = kommendeArrangement[i][0];
+    let slutt = kommendeArrangement[i][1];
+    
+    let startMs = Date.parse(start);
+    let sluttMs = Date.parse(slutt);
+    let naa = Date.now();
+    
+    if(sluttMs > naa && naa >= startMs) {
+        startKlokke(slutt);
+    }
+}
